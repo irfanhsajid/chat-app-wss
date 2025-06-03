283b640fe72683d27889de33227a98d8afbc6aed
irfanhsajid <irfanhsajid@gmail.com>
2025-06-03 19:36:43 +0600

    env updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
+import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: String(process.env.DB_PASSWORD),
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [path.join(__dirname, '/../**/*.entity{.ts,.js}')],
+      migrations: [path.join(__dirname, '/../**/*.entity{.ts,.js}')],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
